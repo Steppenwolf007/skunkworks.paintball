fd324baf8dbba5cbe06d140c00d15ad83284aba3
Alex Pringle <contact@alexpringle.co.uk>
2013-12-04 23:35:48 +0000

    added send json to topic async task

---
+++ SomethingLiberty/SomethingLiberty/src/main/java/com/something/liberty/messaging/SendJsonToTopicTask.java
@@ -0,0 +1,32 @@
+package com.something.liberty.messaging;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+public class SendJsonToTopicTask extends AsyncTask<JSONObject,Void,Void> {
+
+    private String mTopic = null;
+
+    public SendJsonToTopicTask(String topic)
+    {
+        mTopic = topic;
+    }
+
+    @Override
+    protected Void doInBackground(JSONObject... params)
+    {
+        JSONObject jsonMessage = params[0];
+        if(jsonMessage == null)
+        {
+            Log.e("SomethingLiberty","SendJsonToTopicTask : invalid json object " + jsonMessage);
+            return null;
+        }
+        String jsonString = jsonMessage.toString();
+        Log.d("SomethingLiberty","jsonString = " + jsonString);
+        MessagingUtils messagingUtils = MessagingUtils.getMessagingUtils();
+        messagingUtils.sendMesage(mTopic,jsonString);
+        return null;
+    }
+}
