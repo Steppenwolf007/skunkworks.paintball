1a6409d7b39d79e72ddcc9bd9f1423196a1c433c
Alex Pringle <contact@alexpringle.co.uk>
2013-12-09 00:18:25 +0000

    added base NotificationUtils

---
+++ SomethingLiberty/SomethingLiberty/src/main/java/com/something/liberty/alerts/NotificationUtils.java
@@ -0,0 +1,38 @@
+package com.something.liberty.alerts;
+
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.something.liberty.R;
+
+public class NotificationUtils
+{
+    private Context mContext = null;
+
+    public NotificationUtils(Context context)
+    {
+        mContext = context;
+    }
+    
+    public void displayGenericNotification(String titleString,String contentString)
+    {
+        NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Service.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+        builder.setSmallIcon(R.drawable.ic_launcher);
+        builder.setContentTitle(titleString);
+        builder.setContentText(contentString);
+        builder.setTicker(mContext.getString(R.string.notification_ticker));
+
+        Uri notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(notificationSound);
+
+        builder.setVibrate(new long[]{100,100,100,100});
+
+        notificationManager.notify(1,builder.build());
+    }
+
+}
